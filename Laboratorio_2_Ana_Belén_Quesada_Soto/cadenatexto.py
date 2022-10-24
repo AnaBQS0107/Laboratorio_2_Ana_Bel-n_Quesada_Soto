78b7564f7a6e4cc6a273840ade612d5619033b58
AnaBelenUIA <116422089+AnaBelenUIA@users.noreply.github.com>
2022-10-24 13:39:29 -0600

    Delete Laboratorio_2_Ana_Belén_Quesada_Soto directory

--- Laboratorio_2_Ana_Belén_Quesada_Soto/cadenatexto.py
+++
@@ -1,6 +0,0 @@
-
-def contadorletras (Texto, contador):
-      Texto = input ("Digite el texto que desea:  \n\r")
-      contador= Texto.count('a')
-      print ("La letra a se encontró" + contador)
-
